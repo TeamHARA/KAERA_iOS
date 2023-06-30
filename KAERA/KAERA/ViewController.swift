e51340e9ac28734d2f5a4d89820b454feb76e945
daminoworld <ekadls757@gmail.com>
2023-07-01 02:09:13 +0900

    Add: Xcode project files

---
+++ KAERA/KAERA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
