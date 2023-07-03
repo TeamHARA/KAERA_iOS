2f3f94b65b099d71ab237a1b3b54d808b515ed58
daminoworld <ekadls757@gmail.com>
2023-07-03 22:36:20 +0900

    Del: APIConstants Properties

--- KAERA/KAERA/Network/Base/APIConstant.swift
+++ KAERA/KAERA/Network/Base/APIConstant.swift
@@ -17,49 +17,5 @@ import Foundation
 struct APIConstant {
     
     //MARK: - Base
-    static let baseURL = "http://3.37.47.67:3000"
-    
-    //MARK: - Together
-    static let worryVote = "/worry"
-    /// 카테고리별 파라미터 :categoryId
-    static let worryCategory = "/worry"
-    
-    static let worryFinalAlone = "/worry/worryAlone"
-    
-    static let worryFinalWith = "/worry/with"
-    
-    /// 파라미터 :worryId
-    static let worryAloneDetail = "/worry/alone"
-
-    static let worryWithDetail = "/worry/with"
-    
-    //MARK: - Write
-    static let worryAlone = "/worry/alone"
-    
-    static let worryWith = "/worry/with"
-    
-    // MARK: - Storage
-    /// 목록조회 파라미터 :ifSolved
-    static let worryWithList = "/worry/with/list"
-    
-    static let worryAloneList = "/worry/alone/list"
-    
-    static let random = "/random"
-    
-    /// 후순위
-    /// 쿼리
-    static let worrySearchList = "/worry"
-    /// 일단 클라처리
-    static let myPastWorry = "/random/list"
-    
-    
+    static let baseURL = ""
 }
-
-
-
-
-
-
-
-
-
