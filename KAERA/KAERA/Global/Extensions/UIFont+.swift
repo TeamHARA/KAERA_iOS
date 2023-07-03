f58587dbfcf226bd99d356a59bbac5177d83d501
daminoworld <ekadls757@gmail.com>
2023-07-04 02:48:41 +0900

    Add: Custom Fonts Update
    폰트를 적용하는 과정에서 폰트가 family font names list에 제대로 뜨지 않는 에러가 있었으나 otf로 추가한 폰트를 다 삭제하고 ttf로 다시 추가하여 에러를 해결함

--- KAERA/KAERA/Global/Extensions/UIFont+.swift
+++ KAERA/KAERA/Global/Extensions/UIFont+.swift
@@ -9,51 +9,47 @@ import UIKit
 
 extension UIFont {
 
-    class var haraM1M24: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 24.0)!
+    class var kH1B20: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-cBd", size: 20.0)!
     }
     
-    class var haraM1B24: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 24.0)!
+    class var kH2R20: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-bRg", size: 20.0)!
     }
 
-    class var haraM2M18: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 18.0)!
+    class var kH3B18: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-cBd", size: 18.0)!
     }
 
-    class var haraH1Sb16: UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: 16.0)!
+    class var kB1B16: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-cBd", size: 16.0)!
     }
 
-    class var haraH2M16: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 16.0)!
-    }
-
-    class var haraB1Sb14: UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: 14.0)!
-    }
-
-    class var haraB2M14: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 14.0)!
-    }
-
-    class var haraB3R14: UIFont {
-        return UIFont(name: "Pretendard-Regular", size: 14.0)!
-    }
-
-    class var haraSub1Sb12: UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: 12.0)!
-    }
-
-    class var haraSub2M12: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 12.0)!
-    }
-
-    class var haraSub3R12: UIFont {
-        return UIFont(name: "Pretendard-Regular", size: 12.0)!
+    class var kB2M14: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-aLt", size: 14.0)!
     }
     
-    class var haraH1B20: UIFont {
-        return UIFont(name: "Cafe24Ssurround", size: 20.0)!
+    class var kB3B14: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-cBd", size: 14.0)!
+    }
+
+    class var kB4R14: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-bRg", size: 14.0)!
+    }
+
+    class var kB5R14W: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-bRg", size: 14.0)!
+    }
+
+    class var kSb1R12: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-bRg", size: 12.0)!
+    }
+
+    class var kSb2R12W: UIFont {
+        return UIFont(name: "NanumSquareNeoTTF-bRg", size: 12.0)!
+    }
+
+    class var kMoR13: UIFont {
+        return UIFont(name: "NanumMyeongjoOTF", size: 13.0)!
     }
 }
