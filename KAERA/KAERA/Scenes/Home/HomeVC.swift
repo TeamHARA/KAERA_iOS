a12b677f8ab6dee02fb2cca3b8869406d475eb7b
oy6uns <bacho4235@gmail.com>
2023-07-09 21:38:17 +0900

    Feat: #1-빈 VC 추가

--- KAERA/KAERA/Scenes/Home/HomeVC.swift
+++ KAERA/KAERA/Scenes/Home/HomeVC.swift
@@ -11,7 +11,7 @@ class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue//0x1E2227.color
+        view.backgroundColor = .kGray1
         // Do any additional setup after loading the view.
     }
     
