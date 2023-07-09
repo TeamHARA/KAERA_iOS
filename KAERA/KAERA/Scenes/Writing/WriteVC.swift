34ce3f3b2b8f326b3e474330d7ec146aa247bd4a
oy6uns <bacho4235@gmail.com>
2023-07-09 21:34:03 +0900

    Feat: #1-TabBarController 구현

---
+++ KAERA/KAERA/Scenes/Writing/WriteVC.swift
@@ -0,0 +1,29 @@
+//
+//  WorryVC.swift
+//  KAERA
+//
+//  Created by saint on 2023/07/09.
+//
+
+import UIKit
+
+class WorryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue//0x1E2227.color
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
