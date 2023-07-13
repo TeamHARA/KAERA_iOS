982f9a765314846cd86f654a441b043c8ef89caf
oy6uns <bacho4235@gmail.com>
2023-07-13 23:13:51 +0900

    Feat: #12-WorryListModel 구현

--- KAERA/KAERA/Scenes/Writing/Model/WorryListModel.swift
+++ KAERA/KAERA/Scenes/Writing/Model/WorryListModel.swift
@@ -1,8 +1,25 @@
 //
-//  ArchiveModel.swift
+//  WorryListModel.swift
 //  KAERA
 //
 //  Created by 김담인 on 2023/07/13.
 //
 
-import Foundation
+import UIKit
+
+// 서버통신용 model(codable)
+struct WorryListModel{
+    let templateId: Int
+    let templateTitle: String
+    let title: String
+    let period: String
+}
+
+// View에 뿌려주기 위한 model
+struct WorryListPublisherModel{
+    let templateId: Int
+    let templateTitle: String
+    let title: String
+    let period: String
+    let image: UIImage
+}
