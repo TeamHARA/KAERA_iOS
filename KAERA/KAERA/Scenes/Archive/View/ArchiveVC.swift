ec2e5b6d7bbab203e69ea726e161827ca8e4d62b
oy6uns <bacho4235@gmail.com>
2023-07-13 23:14:29 +0900

    Feat: #12-고민보관함 고민 컬렉션뷰 구현

--- KAERA/KAERA/Scenes/Archive/View/ArchiveVC.swift
+++ KAERA/KAERA/Scenes/Archive/View/ArchiveVC.swift
@@ -6,24 +6,117 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import Combine
+
 
 class ArchiveVC: UIViewController {
+    
+    // MARK: - Properties
+    private let sortHeaderView = ArchiveHeaderView()
+    
+    var worryVM: WorryListViewModel = WorryListViewModel()
+    var worryList: [WorryListPublisherModel] = []
+    var disposalbleBag = Set<AnyCancellable>()
+    
+    private let flowLayout = UICollectionViewFlowLayout().then{
+        $0.scrollDirection = .vertical
+    }
+    
+    lazy var worryListCV: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: self.flowLayout)
+        view.showsHorizontalScrollIndicator = false
+        view.backgroundColor = .clear
+        view.delegate = self
+        view.dataSource = self
+        return view
+    }()
+    
+    // MARK: - Constants
+    final let worryListInset: UIEdgeInsets = UIEdgeInsets(top: 0, left: 16.adjustedW, bottom: 100, right: 16.adjustedW)
+    final let interItemSpacing: CGFloat = 12.adjustedW
+    final let lineSpacing: CGFloat = 12.adjustedW
 
+    // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .kGray1
-        // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = true
+        setLayout()
+        registerCV()
+        setBindings()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func registerCV() {
+        worryListCV.register(WorryListCVC.self,
+                             forCellWithReuseIdentifier: WorryListCVC.classIdentifier)
+//        worryListCV.register(ArchiveHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: ArchiveHeaderView.classIdentifier)
     }
-    */
-
 }
+
+// MARK: - Layout
+extension ArchiveVC {
+    private func setLayout() {
+        view.backgroundColor = .kGray1
+        view.addSubviews([sortHeaderView, worryListCV])
+        
+        sortHeaderView.snp.makeConstraints{
+            $0.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+            $0.height.equalTo(152.adjustedW)
+        }
+        
+        worryListCV.snp.makeConstraints{
+            $0.top.equalTo(sortHeaderView.snp.bottom)
+            $0.leading.trailing.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+// MARK: - 뷰모델 관련
+extension ArchiveVC{
+    /// 뷰모델의 데이터를 뷰컨의 리스트 데이터와 연동
+    fileprivate func setBindings(){
+        print("ViewController - setBindings()")
+        self.worryVM.worryListPublisher.sink{ [weak self] (updatedList : [WorryListPublisherModel]) in
+            print("ViewController - updatedList.count: \(updatedList.count)")
+            self?.worryList = updatedList
+            self?.sortHeaderView.numLabel.text = "총 \(self!.worryList.count)개"
+        }.store(in: &disposalbleBag)
+    }
+}
+
+// MARK: - UICollectionDelegate
+extension ArchiveVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 165.adjustedW, height: 165.adjustedW)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return lineSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return interItemSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return worryListInset
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension ArchiveVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return worryList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: WorryListCVC.classIdentifier, for: indexPath)
+                as? WorryListCVC else { return UICollectionViewCell() }
+        cell.dataBind(model: worryList[indexPath.item])
+        return cell
+    }
+}
+
 
--- KAERA/KAERA/Scenes/Archive/View/WorryListCVC.swift
+++ KAERA/KAERA/Scenes/Archive/View/WorryListCVC.swift
@@ -5,4 +5,90 @@
 //  Created by saint on 2023/07/13.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class WorryListCVC: UICollectionViewCell {
+    
+    private let worryCell = UIImageView().then{
+        $0.image = UIImage(named: "framebg")
+        $0.backgroundColor = .clear
+    }
+    
+    private let jewelImage = UIImageView().then{
+        $0.image = UIImage(named: "jewel")
+        $0.backgroundColor = .clear
+    }
+    
+    private let worryTitle = UILabel().then{
+        $0.text = "고민 제목입니당"
+        $0.font = .kB3B14
+        $0.textColor = .white
+    }
+    
+    private let divLine = UIImageView().then{
+        $0.image = UIImage(named: "icn_div_line")
+        $0.backgroundColor = .clear
+    }
+
+    private let worryDate = UILabel().then{
+        $0.text = "2023.02.10~2023.04.01"
+        $0.font = .kSb1R12
+        $0.textColor = .kGray4
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension WorryListCVC{
+    
+    // MARK: - Layout
+    private func setLayout() {
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        contentView.addSubview(worryCell)
+        
+        worryCell.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+        
+        worryCell.addSubviews([jewelImage, worryTitle, divLine, worryDate])
+        
+        jewelImage.snp.makeConstraints{
+            $0.width.height.equalTo(80.adjustedW)
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(8.adjustedW)
+        }
+        
+        worryTitle.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(jewelImage.snp.bottom).offset(10.adjustedW)
+        }
+        
+        divLine.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(worryTitle.snp.bottom).offset(8.adjustedW)
+            $0.width.equalTo(140.adjustedW)
+            $0.height.equalTo(5)
+        }
+        
+        worryDate.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(divLine.snp.bottom).offset(8.adjustedW)
+        }
+    }
+    
+    func dataBind(model: WorryListPublisherModel) {
+        jewelImage.image = model.image
+        worryTitle.text = model.title
+        worryDate.text = model.period
+    }
+}
