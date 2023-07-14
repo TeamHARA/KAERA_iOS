363e05f739ef5c3bb686403e4a33473456dd4b2c
oy6uns <bacho4235@gmail.com>
2023-07-14 18:18:44 +0900

    Add: #16-TableView 셀 및 Header 배경색, 보석 이미지 추가

---
+++ KAERA/KAERA/Scenes/Archive/View/ArchiveTemplateInfo/TemplateInfoHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  TemplateInfoHeaderView.swift
+//  KAERA
+//
+//  Created by saint on 2023/07/14.
+//
+
+import Foundation
---
+++ KAERA/KAERA/Scenes/Archive/View/ArchiveTemplateInfo/TemplateInfoTVC.swift
@@ -0,0 +1,8 @@
+//
+//  TemplateInfoTVC.swift
+//  KAERA
+//
+//  Created by saint on 2023/07/14.
+//
+
+import Foundation
