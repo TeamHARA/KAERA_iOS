4d825b798cbf1ce9ff71fe7cf9f176769501ff96
oy6uns <bacho4235@gmail.com>
2023-07-14 18:21:54 +0900

    Feat: #16-Template 설명 TableView UI구현

--- KAERA/KAERA/Scenes/Archive/View/ArchiveTemplateInfo/TemplateInfoVC.swift
+++ KAERA/KAERA/Scenes/Archive/View/ArchiveTemplateInfo/TemplateInfoVC.swift
@@ -24,11 +24,28 @@ class TemplateInfoVC: UIViewController {
         $0.contentMode = .scaleToFill
         $0.backgroundColor = .clear
     }
-
+    
+    private let headerView = TemplateInfoHeaderView()
+    
+    private lazy var templateInfoTV = UITableView(frame: .zero, style: .grouped).then {
+        $0.backgroundColor = .kGray1
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.separatorStyle = .none
+        $0.showsVerticalScrollIndicator = false
+        $0.delegate = self
+        $0.dataSource = self
+    }
+    
+    var expandedCells = [Bool]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
         pressBtn()
+        registerTV()
+        expandedCells = Array(repeating: false, count: 6)  // Initialize with the number of cells
+//        templateInfoTV.rowHeight = UITableView.automaticDimension
+//        templateInfoTV.estimatedRowHeight = 70  // or whatever your estimated height is
     }
     
     // MARK: - Functions
@@ -37,13 +54,18 @@ class TemplateInfoVC: UIViewController {
             self.navigationController?.popViewController(animated: true)
         }
     }
+    
+    private func registerTV() {
+        templateInfoTV.register(TemplateInfoTVC.self,
+                                forCellReuseIdentifier: TemplateInfoTVC.classIdentifier)
+    }
 }
 
 // MARK: - Layout
 extension TemplateInfoVC{
     func setLayout() {
         view.backgroundColor = .kGray1
-        view.addSubViews([titleLabel, backBtn])
+        view.addSubViews([titleLabel, backBtn, templateInfoTV])
         
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(24.adjustedW)
@@ -56,5 +78,62 @@ extension TemplateInfoVC{
             $0.width.equalTo(24)
             $0.height.equalTo(24)
         }
+        
+        templateInfoTV.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(20.adjustedW)
+            $0.leading.equalTo(self.view.safeAreaLayoutGuide).offset(16.adjustedW)
+            $0.trailing.equalTo(self.view.safeAreaLayoutGuide).offset(-16.adjustedW)
+            $0.bottom.equalToSuperview().offset(-20.adjustedW)
+        }
+        
+        templateInfoTV.translatesAutoresizingMaskIntoConstraints = false
+        templateInfoTV.tableHeaderView = headerView
+        headerView.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(342.adjustedW)
+            $0.height.equalTo(168.adjustedW)
+        }
+        headerView.layoutIfNeeded()
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension TemplateInfoVC: UITableViewDelegate {
+    
+    /// tableView와 Cell간에 간격을 주기 위해 빈 뷰를 추가
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        expandedCells[indexPath.row] = !expandedCells[indexPath.row]
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+        print(expandedCells)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension TemplateInfoVC : UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TemplateInfoTVC.classIdentifier, for: indexPath) as? TemplateInfoTVC else {return UITableViewCell()}
+        
+        cell.settingData(isExpanded: expandedCells[indexPath.row])
+        cell.layoutIfNeeded()
+        return cell
     }
 }
