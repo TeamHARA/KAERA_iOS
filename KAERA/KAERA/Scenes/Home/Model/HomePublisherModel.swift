1932643e62c70fa2592df45866b20e60bf6c5f08
daminoworld <ekadls757@gmail.com>
2023-07-20 18:28:59 +0900

    Add: #19-Create HomePublisherModel

---
+++ KAERA/KAERA/Scenes/Home/Model/HomePublisherModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomePublisherModel.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/07/20.
+//
+
+import Foundation
+
+struct HomePublisherModel: Codable {
+    let worryId: Int
+    let templateId: Int
+    let imageName: String
+    let title: String
+}
