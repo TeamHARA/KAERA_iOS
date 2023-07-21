6d6176a719efad9356194d4f8df2c31fdc002c19
daminoworld <ekadls757@gmail.com>
2023-07-21 21:26:55 +0900

    Add: #21-Create HomeWorryDetailTVC

---
+++ KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/HomeWorryDetailTVC.swift
@@ -0,0 +1,21 @@
+//
+//  HomeWorryDetailTVC.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/07/21.
+//
+
+import UIKit
+
+class HomeWorryDetailTVC: UITableViewCell {
+
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
