afc5dd9e30e78175797f754faed904bb68dbc86f
daminoworld <ekadls757@gmail.com>
2023-07-21 22:06:15 +0900

    Feat: #21-Setup TableView in HomeWorryDetailVC

--- KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/HomeWorryDetailVC.swift
+++ KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/HomeWorryDetailVC.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 import SnapKit
+import Then
 
 final class HomeWorryDetailVC: UIViewController {
     
     // MARK: - Properties
     private let navigationBarView = CustomNavigationBarView(leftType: .close, rightType: .edit, title: "고민캐기")
     private var deadLineDays = 1
+    private let backGroundImageView = UIImageView().then {
+        $0.image = UIImage(named: "framebg")
+    }
+    private let worryDetailTV = UITableView().then {
+        $0.estimatedRowHeight = 93
+        $0.rowHeight = UITableView.automaticDimension
+        $0.backgroundColor = .clear
+    }
     
     // MARK: - Initialization
     override func viewDidLoad() {
@@ -20,8 +29,9 @@ final class HomeWorryDetailVC: UIViewController {
         self.view.backgroundColor = .kGray1
         setLayout()
         setNaviButtonAction()
-        /// 서버에서 넘어오는 데드라인 값을 넣어 실행
+        //TODO: 서버에서 넘어오는 데드라인 값을 넣어 실행
         navigationBarView.setTitleText(text: "고민캐기 D-\(deadLineDays)")
+        setupTableView()
     }
     
     // MARK: - Function
@@ -31,21 +41,51 @@ final class HomeWorryDetailVC: UIViewController {
         }
         
         navigationBarView.setRightButtonAction {
-            /// edit 창 띄우기
+            //TODO: edit 창 띄우기
         }
     }
+    
+    private func setupTableView() {
+        worryDetailTV.backgroundView = backGroundImageView
+        worryDetailTV.dataSource = self
+        worryDetailTV.delegate = self
+        worryDetailTV.register(HomeWorryDetailTVC.self, forCellReuseIdentifier: HomeWorryDetailTVC.className)
+    }
+}
+
+extension HomeWorryDetailVC: UITableViewDelegate {
+    
+}
+
+extension HomeWorryDetailVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeWorryDetailTVC.className) as? HomeWorryDetailTVC else { return UITableViewCell() }
+        return cell
+    }
+    
     
 }
 
 // MARK: - UI
 extension HomeWorryDetailVC {
     private func setLayout() {
-        self.view.addSubviews([navigationBarView])
+        self.view.addSubviews([navigationBarView, worryDetailTV])
         
         navigationBarView.snp.makeConstraints {
             $0.left.right.equalToSuperview().inset(16)
             $0.top.equalToSuperview().inset(70)
-            $0.height.equalTo(24)
+            $0.height.equalTo(50)
+        }
+        
+        worryDetailTV.snp.makeConstraints {
+            $0.directionalHorizontalEdges.equalToSuperview().inset(16)
+            $0.top.equalTo(navigationBarView.snp.bottom)
+            $0.bottom.equalToSuperview()
         }
     }
 }
