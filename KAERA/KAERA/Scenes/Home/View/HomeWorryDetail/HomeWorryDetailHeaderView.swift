069a2464e1f9e34c917ca1d35dc1522083c30081
daminoworld <ekadls757@gmail.com>
2023-07-22 10:48:46 +0900

    Feat: #21-Create HomeWorryDetailHeaderView

---
+++ KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/HomeWorryDetailHeaderView.swift
@@ -0,0 +1,52 @@
+//
+//  HomeWorryDetailHeaderView.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/07/22.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HomeWorryDetailHeaderView: UITableViewHeaderFooterView {
+    
+    // MARK: - Properties
+    private let gemStoneImageView = UIImageView().then {
+        $0.image = UIImage(named: "gemstone_blue")
+    }
+    
+    private let worryTitle = UILabel().then {
+        $0.textColor = .kWhite
+        $0.font = .kH3B18
+        $0.text = "고민 제목"
+        $0.textAlignment = .center
+    }
+    
+    // MARK: - Initialization
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Function
+    private func setLayout() {
+        self.addSubviews([gemStoneImageView, worryTitle])
+        
+        gemStoneImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(15)
+            $0.height.width.equalTo(80.adjustedH)
+        }
+        
+        worryTitle.snp.makeConstraints {
+            $0.top.equalTo(gemStoneImageView.snp.bottom).offset(24)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+}
