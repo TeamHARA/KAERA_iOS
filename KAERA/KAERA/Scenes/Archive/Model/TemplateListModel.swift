ca9c728c0d39c83063276a0fcadeb59add2f23e2
oy6uns <bacho4235@gmail.com>
2023-07-28 01:23:30 +0900

    Feat: #23-서버통신용 Codable 모델 수정 및 TemplateInfoPublisherModel 구현

--- KAERA/KAERA/Scenes/Archive/Model/TemplateListModel.swift
+++ KAERA/KAERA/Scenes/Archive/Model/TemplateListModel.swift
@@ -9,10 +9,11 @@ import UIKit
 import Foundation
 
 /// 서버통신용 모델
-struct TemplateListModel {
+struct TemplateListModel: Codable {
     let templateId: Int
     let templateTitle: String
-    let templateDetail: String
+    let shortInfo: String
+    let info: String
     let hasUsed: Bool
 }
 
@@ -21,5 +22,13 @@ struct TemplateListPublisherModel {
     let templateId: Int
     let templateTitle: String
     let templateDetail: String
+    let image: UIImage
+}
+
+/// View에 뿌려주기 위한 model
+struct TemplateInfoPublisherModel {
+    let templateId: Int
+    let templateTitle: String
+    let info: String
     let image: UIImage
 }
