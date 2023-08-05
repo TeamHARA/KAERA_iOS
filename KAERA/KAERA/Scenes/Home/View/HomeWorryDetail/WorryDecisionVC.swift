76044a0e379daeab6303d5b42faa087065c66451
daminoworld <ekadls757@gmail.com>
2023-08-05 21:11:01 +0900

    Feat: #27-Set Dynamic UI for Keyboard WIndow

--- KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/WorryDecisionVC.swift
+++ KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/WorryDecisionVC.swift
@@ -51,6 +51,11 @@ final class WorryDecisionVC: BaseVC {
         $0.titleLabel?.textAlignment = .center
         $0.layer.cornerRadius = 8
     }
+    
+    private let topInset: CGFloat = 303
+    private var hasKeyboard: Bool = false
+    private let placeholderText = "40자 이내로 적어주세요."
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,18 +64,92 @@ final class WorryDecisionVC: BaseVC {
         hideKeyboardWhenTappedAround()
         setTextView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.addKeyboardObserver()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.removeKeyboardObserver()
+    }
+    
+    // MARK: - Function
+    private func setTextView() {
+        worryTextView.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
+}
+// MARK: - Keyboard Setting
+extension WorryDecisionVC {
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.keyboardWillAppear(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillDisappear),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    
+    private func removeKeyboardObserver() {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nibName
+        )
+        
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nibName
+        )
+    }
+    
+    @objc
+    func keyboardWillAppear(_ notification: NSNotification) {
+        hasKeyboard = true
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                UIView.animate(withDuration: 0.1, animations: { [self] in
+                    let newInset = abs(topInset - (keyboardSize.height - 100))
+                    mainView.snp.updateConstraints {
+                        $0.top.equalTo(view.safeAreaLayoutGuide).inset(newInset.adjustedH)
+                    }
+                })
+        }
+    }
+    @objc
+    func keyboardWillDisappear(_ notification: NSNotification) {
+        UIView.animate(withDuration: 0.1, animations: { [self] in
+            mainView.snp.updateConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide).inset(topInset.adjustedH)
+            }
+        })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        guard let touch = touches.first else {
+            return
+        }
+        
+        /// 배경 영역을 탭한 경우 뷰 컨트롤러를 닫음
+        if !mainView.frame.contains(touch.location(in: view)) && !hasKeyboard {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.dismiss(animated: true, completion: nil)
+            })
+        }
+        
+        ///  키보드가 내려가 있었을 때와 키보드가 올라왔있다가 내려가고 호출되어 false 고정
+        hasKeyboard = false
+    }
+}
 
 // MARK: - UI
 extension WorryDecisionVC {
