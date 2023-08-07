7859097a7f1e611af55eaf1afaa81be964b1505b
daminoworld <ekadls757@gmail.com>
2023-08-08 08:49:08 +0900

    Add: #27-Create WorryQuoteView

---
+++ KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/WorryQuoteView.swift
@@ -0,0 +1,20 @@
+//
+//  WorryQuoteView.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/08/07.
+//
+
+import UIKit
+
+class WorryQuoteView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
