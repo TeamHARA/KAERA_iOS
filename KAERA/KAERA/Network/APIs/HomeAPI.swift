84bc20103df22409bfbbc0c5995fde8f411312e7
daminoworld <ekadls757@gmail.com>
2023-08-28 23:40:28 +0900

    Feat: #35-Make getWorryDetail func in Home API

--- KAERA/KAERA/Network/APIs/HomeAPI.swift
+++ KAERA/KAERA/Network/APIs/HomeAPI.swift
@@ -16,7 +16,7 @@ final class HomeAPI {
     private init() { }
     
     public private(set) var homeGemListResponse: GeneralArrayResponse<HomeGemListModel>?
-    
+    public private(set) var worryDetailResponse: GeneralResponse<WorryDetailModel>?
    
     // MARK: - HomeGemList
     func getHomeGemList(param: Int, completion: @escaping (GeneralArrayResponse<HomeGemListModel>?) -> () ) {
@@ -26,11 +26,28 @@ final class HomeAPI {
                 do {
                     self?.homeGemListResponse = try
                     result.map(GeneralArrayResponse<HomeGemListModel>?.self)
-                    print("성공")
-                    print(result)
                     guard let gemList = self?.homeGemListResponse else { return }
-                    print(gemList)
                     completion(gemList)
+                } catch(let err) {
+                    print(err.localizedDescription)
+                    completion(nil)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+    
+    // MARK: - WorryDetail
+    func getWorryDetail(param: Int, completion: @escaping (GeneralResponse<WorryDetailModel>?) -> () ) {
+        homeProvider.request(.worryDetail(worryId: param)) { [weak self] response in
+            switch response {
+            case .success(let result):
+                do {
+                    self?.worryDetailResponse = try result.map(GeneralResponse<WorryDetailModel>?.self)
+                    guard let worryDetail = self?.worryDetailResponse else { return }
+                    completion(worryDetail)
                 } catch(let err) {
                     print(err.localizedDescription)
                     completion(nil)
