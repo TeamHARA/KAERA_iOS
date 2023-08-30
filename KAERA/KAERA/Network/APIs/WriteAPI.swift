e290bd035b8a82216321d764e3cb721976c9e682
oy6uns <bacho4235@gmail.com>
2023-08-30 14:25:31 +0900

    #42: Feat-WriteAPI 구현

--- KAERA/KAERA/Network/APIs/WriteAPI.swift
+++ KAERA/KAERA/Network/APIs/WriteAPI.swift
@@ -12,16 +12,17 @@ import Moya
 final class WriteAPI {
     
     static let shared: WriteAPI = WriteAPI()
-    private let archiveProvider = MoyaProvider<WriteService>(plugins: [MoyaLoggingPlugin()])
+    private let writeProvider = MoyaProvider<WriteService>(plugins: [MoyaLoggingPlugin()])
     
     private init() { }
     
     public private(set) var templateListResponse: GeneralArrayResponse<TemplateListModel>?
+    public private(set) var templateContentResponse: GeneralResponse<TemplateContentModel>?
     
     
     // MARK: - HomeGemList
     func getTemplateList(completion: @escaping (GeneralArrayResponse<TemplateListModel>?) -> () ) {
-        archiveProvider.request(.getTemplateList) { [weak self] response in
+        writeProvider.request(.getTemplateList) { [weak self] response in
             switch response {
             case .success(let result):
                 do {
@@ -32,6 +33,29 @@ final class WriteAPI {
                     guard let templateList = self?.templateListResponse else { return }
                     print(templateList)
                     completion(templateList)
+                } catch(let err) {
+                    print(err.localizedDescription)
+                    completion(nil)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+    
+    func getTemplateQuestion(param: Int, completion: @escaping (GeneralResponse<TemplateContentModel>?) -> () ) {
+        writeProvider.request(.getTemplateQuestion(templateId: param)) { [weak self] response in
+            switch response {
+            case .success(let result):
+                do {
+                    self?.templateContentResponse = try
+                    result.map(GeneralResponse<TemplateContentModel>?.self)
+                    print("성공")
+                    print(result)
+                    guard let contentList = self?.templateContentResponse else { return }
+                    print(contentList)
+                    completion(contentList)
                 } catch(let err) {
                     print(err.localizedDescription)
                     completion(nil)
