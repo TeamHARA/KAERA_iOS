d67fd5b84c8c1c5f62d453722c60cc3ecc72c596
daminoworld <ekadls757@gmail.com>
2023-09-09 13:06:37 +0900

    Feat: #47-Add kakaoAppKey info from Bundle

--- KAERA/KAERA/Application/Environment.swift
+++ KAERA/KAERA/Application/Environment.swift
@@ -8,4 +8,10 @@
 import Foundation
 
 struct Environment {
+    static let kakaoAppKey = {
+        guard let appKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as? String else {
+            fatalError("KAKAO APP KEY not set in plist for this Environment")
+        }
+        return appKey
+    }()
 }
