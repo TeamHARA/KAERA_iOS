a88fef41034eb22b6ecb5d2b6f7504ac87ff016c
daminoworld <ekadls757@gmail.com>
2023-10-08 18:24:11 +0900

    Feat: #71-Implement renewal refresh token API
    - data.status 코드가 200 일때를 제외하고 nil을 completion으로 전달하여 SplashVC에서 SignInVC로 넘어가도록 함

--- KAERA/KAERA/Network/APIs/AuthAPI.swift
+++ KAERA/KAERA/Network/APIs/AuthAPI.swift
@@ -16,6 +16,7 @@ final class AuthAPI {
     private init() { }
     
     public private(set) var kakaoLoginResponse: GeneralResponse<SignInModel>?
+    public private(set) var renewalResponse: GeneralResponse<RenewalTokenModel>?
     
     func postKakaoLogin(token: String, completion: @escaping (GeneralResponse<SignInModel>?) -> ()) {
         authProvider.request(.kakaoLogin(token: token)) { [weak self] response in
@@ -30,7 +31,30 @@ final class AuthAPI {
                     completion(nil)
                 }
             case .failure(let err):
-                print("에러!")
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+    
+    func postRenewalRequest(completion: @escaping (GeneralResponse<RenewalTokenModel>?) -> ()) {
+        authProvider.request(.renewelToken) { [weak self] response in
+            switch response {
+            case .success(let result):
+                do {
+                    self?.renewalResponse = try result.map(GeneralResponse<RenewalTokenModel>?.self)
+                    guard let data = self?.renewalResponse else { return }
+                    switch data.status {
+                    case 200:
+                        completion(data)
+                    default:
+                        completion(nil)
+                    }
+                } catch(let err) {
+                    print(err.localizedDescription)
+                    completion(nil)
+                }
+            case .failure(let err):
                 print(err.localizedDescription)
                 completion(nil)
             }
