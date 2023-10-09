15ecac88b7911771679d80666d5bd68b64b6106d
oy6uns <bacho4235@gmail.com>
2023-10-09 16:42:47 +0900

    Feat: #64-서버 통신 성공, 실패 시에 각각 다른 알럿 창 띄워주기

--- KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/HomeWorryEditVC.swift
+++ KAERA/KAERA/Scenes/Home/View/HomeWorryDetail/HomeWorryEditVC.swift
@@ -66,7 +66,7 @@ final class HomeWorryEditVC: BaseVC {
     private func dismissVC() {
         self.dismiss(animated: true)
     }
-
+    
     private func setPressAction() {
         editWorryButton.press {
             //TODO: 고민 수정하기 -> 수정용 작성페이지(고민 상세에 있던 데이터 전달)
@@ -87,18 +87,40 @@ final class HomeWorryEditVC: BaseVC {
         }
         
         deleteWorryButton.press {
-            /// 버튼, 타이틀, 서브타이틀, 강조 텍스트, 버튼 액션 추가 가능
+            /// 서버통신 성공 시 띄울 창 구현
             let alertVC = KaeraAlertVC(okTitle: "삭제")
             alertVC.setTitleSubTitle(title: "고민을 삭제하시나요?", subTitle: "삭제된 고민은 복구할 수 없어요", highlighting: "삭제")
-            alertVC.OKButton.press {
-                /// 고민 삭제 delete 서버 통신 구현
-                HomeAPI.shared.deleteWorryResponse(param: self.worryId)
-                self.dismiss(animated: true, completion: {
-                    /// 현재 뷰에서 present 되있는 뷰를 dismiss 해준다.
-                    self.presentingViewController?.dismiss(animated: true, completion: {
-                        NotificationCenter.default.post(name: NSNotification.Name("dismissAlertVC"), object: nil, userInfo: nil)
-                    })
-                })
+            
+            /// 서버통신 실패 시 띄울 알럿 창 구현
+            let failureAlertVC = KaeraAlertVC(buttonType: .onlyOK, okTitle: "확인")
+            failureAlertVC.setTitleSubTitle(title: "삭제에 실패했어요", subTitle: "다시 한번 시도해주세요.", highlighting: "삭제")
+            
+            alertVC.OKButton.press { [weak self] in
+                /// 서버통신 성공 시 창 모두 dismiss
+                self?.deleteWorry { success in
+                    if success {
+                        self?.dismiss(animated: true) { [weak self] in
+                            if let worryEditVC = self?.presentingViewController,
+                               let worryDetail = worryEditVC.presentingViewController {
+                                worryEditVC.dismiss(animated: true) {
+                                    worryDetail.dismiss(animated: true)
+                                }
+                            }
+                        }
+                    /// 서버통신 실패 시 "삭제 실패" 알럿창 띄우기
+                    } else {
+                        self?.dismiss(animated: true) { [weak self] in
+                            if let worryEditVC = self?.presentingViewController {
+                                worryEditVC.dismiss(animated: true)
+                            }
+                        }
+                        /// "삭제 실패" 알럿창 띄운 뒤 확인 버튼 클릭 시 창 닫히게 구현
+                        self?.present(failureAlertVC, animated: true)
+                        failureAlertVC.OKButton.press {
+                            self?.dismiss(animated: true)
+                        }
+                    }
+                }
             }
             self.present(alertVC, animated: true)
         }
