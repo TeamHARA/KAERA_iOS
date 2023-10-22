f6ced02942efa97b99ddfb56a060578aeb9ae3ec
daminoworld <ekadls757@gmail.com>
2023-10-22 15:13:37 +0900

    Add: #73-Add MyPageTVC Model
    - 처음 뷰 로드시 VM에서 받아서 셀에 넘겨줄 데이터의의 모델을 따로 정의

---
+++ KAERA/KAERA/Scenes/Auth/Model/MyPageTVCModel.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageTVCModel.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/10/17.
+//
+
+import Foundation
+
+struct MyPageTVCModel {
+    let headerTitle: String
+    let rowTitles: [String]
+    let rowButton: MyPageButtonType
+}
