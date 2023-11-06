d1504fc1a480ec4fc23ce8769a712fbe373cf0cc
daminoworld <ekadls757@gmail.com>
2023-11-07 08:57:42 +0900

    Add: #89-Add Singleton for Counting GemStone

---
+++ KAERA/KAERA/Scenes/Home/Model/HomeGemStoneCount.swift
@@ -0,0 +1,17 @@
+//
+//  HomeGemStoneCount.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/11/07.
+//
+
+import Foundation
+
+class HomeGemStoneCount {
+    
+    static let shared = HomeGemStoneCount()
+    
+    var count: Int = 0
+    
+    private init() {}
+}
