646bc041a0d53dbdd3acf23aca41d1487b5b021f
daminoworld <ekadls757@gmail.com>
2023-12-04 00:39:02 +0900

    Feat: #113-Add Error Type for TemplateContentVM

--- KAERA/KAERA/Scenes/ViewModel/TemplateContentViewModel.swift
+++ KAERA/KAERA/Scenes/ViewModel/TemplateContentViewModel.swift
@@ -12,12 +12,12 @@ import Combine
 class TemplateContentViewModel: ViewModelType {
     
     typealias Input = AnyPublisher<Int, Never>
-    typealias Output = AnyPublisher<TemplateContentModel, Never>
+    typealias Output = AnyPublisher<TemplateContentModel, Error>
     
     private var cancellables = Set<AnyCancellable>()
-    private let output = PassthroughSubject<TemplateContentModel, Never> ()
+    private let output = PassthroughSubject<TemplateContentModel, Error> ()
     
-    func transform(input: Input) -> AnyPublisher<TemplateContentModel, Never> {
+    func transform(input: Input) -> Output {
         input.sink{[weak self] templateId in
             self?.getTemplateContents(templateId)
         }
@@ -31,7 +31,10 @@ extension TemplateContentViewModel {
     private func getTemplateContents(_ templateId: Int) {
         /// 서버 통신으로 template Id request 후에 데이터 가져오기
         WriteAPI.shared.getTemplateQuestion(param: templateId) { result in
-            guard let result = result, let data = result.data else { return }
+            guard let result = result, let data = result.data else {
+                self.output.send(completion: .failure(NSError()))
+                return
+            }
             self.output.send(data)
         }
     }
