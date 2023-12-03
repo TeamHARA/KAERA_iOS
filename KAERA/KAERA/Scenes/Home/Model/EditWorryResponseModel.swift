59021dcee130bc809ebdad39722ba8cab7aa6308
daminoworld <ekadls757@gmail.com>
2023-12-03 19:56:46 +0900

    Fix: #113-Add EditWorryResponseModel
    - 기존에 고민수정 리스폰스 data의 모델을 따로 구조체로 만들지 않고 String으로 지정해서 리스폰스의 data를 바인디하는 과정에서 에러 처리가 되는 문제가 있었음 (리스폰스의 모델의 프로퍼티가 updataAt: String)하나만 있어서 그렇게 만든것으로 보임
    - 해당 리스폰스 data를 위한 edtiResponseModel을 Codable로 만들어 주고 해당 모델로 HomeAPI내의 editWorry 리스폰스 모델 교체

---
+++ KAERA/KAERA/Scenes/Home/Model/EditWorryResponseModel.swift
@@ -0,0 +1,12 @@
+//
+//  EditWorryResponseModel.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/12/03.
+//
+
+import Foundation
+
+struct EditWorryResponseModel: Codable {
+    let updatedAt: String
+}
