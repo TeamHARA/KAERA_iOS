9381ca033b0af87c51fceb00dc12f74b03f6b41d
daminoworld <ekadls757@gmail.com>
2023-12-26 18:17:11 +0900

    Add: #148-Add deadline update response model

---
+++ KAERA/KAERA/Scenes/Home/Model/WorryDeadlineUpdateResponseModel.swift
@@ -0,0 +1,13 @@
+//
+//  WorryDeadlineUpdateResponseModel.swift
+//  KAERA
+//
+//  Created by 김담인 on 2023/12/26.
+//
+
+import Foundation
+
+struct WorryDeadlineUpdateResponseModel: Codable {
+    let deadline: String
+    let dDay: Int
+}
