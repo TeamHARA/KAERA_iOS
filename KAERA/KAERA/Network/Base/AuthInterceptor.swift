26a8c983e49168f38f95f517a48e222c6e82716a
daminoworld <ekadls757@gmail.com>
2024-01-09 23:57:25 +0900

    Feat: #184-Add renewedToken header using adapt
    - retry에 진입해 토큰 갱신 API를 통해 토큰을 갱신해도 기존 만료된 엑세스 토큰을 가진 요청의 헤더의  엑세스 토큰은 바뀌지 않아 다시 retry로 계속 진입하게 되는 문제가 있었음
    - 그래서 hasAccessTokenRenewed 플래그를 두고 토큰이 갱신되었을 때 API 요청 직전에 실행되는 adapt 메서드를 구현해 기존 요청의 헤더 값을 갱신된 엑세스 토큰으로 수정해주고 다시 요청을 수행하도록 문제를 해결

--- KAERA/KAERA/Network/Base/AuthInterceptor.swift
+++ KAERA/KAERA/Network/Base/AuthInterceptor.swift
@@ -16,6 +16,20 @@ final class AuthInterceptor: RequestInterceptor {
     private init() {}
 
     private let retryLimit = 5
+    private var hasAccessTokenRenewed = false
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        if hasAccessTokenRenewed {
+            var renewedUrlRequest = urlRequest
+            if let accessToken = KeychainManager.load(key: .accessToken) {
+                renewedUrlRequest.setValue(accessToken, forHTTPHeaderField: "Authorization")
+                hasAccessTokenRenewed = false
+                completion(.success(renewedUrlRequest))
+                return
+            }
+        }
+        completion(.success(urlRequest))
+    }
     
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         print("retry 진입")
@@ -31,7 +45,7 @@ final class AuthInterceptor: RequestInterceptor {
             completion(.doNotRetryWithError(error))
             return
         }
-        AuthAPI.shared.postRenewalRequest { res in
+        AuthAPI.shared.postRenewalRequest { [weak self] res in
             guard let res, let data = res.data else {
                 //TODO: 재갱신 실패시 Refresh Token까지 만료된 것이라 처리 필요
                 completion(.doNotRetryWithError(error))
@@ -39,6 +53,7 @@ final class AuthInterceptor: RequestInterceptor {
             }
             let renewedAccessToken = data.accessToken
             KeychainManager.save(key: .accessToken, value: renewedAccessToken)
+            self?.hasAccessTokenRenewed = true
             completion(.retry)
         }
     }
