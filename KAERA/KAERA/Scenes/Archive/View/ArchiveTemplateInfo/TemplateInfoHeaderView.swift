5d09c8e80c62e7e96a9d75aecbef4cb10d4cd769
oy6uns <bacho4235@gmail.com>
2024-01-16 00:24:51 +0900

    Chore: #179-modify Button BackgroundImg

--- KAERA/KAERA/Scenes/Archive/View/ArchiveTemplateInfo/TemplateInfoHeaderView.swift
+++ KAERA/KAERA/Scenes/Archive/View/ArchiveTemplateInfo/TemplateInfoHeaderView.swift
@@ -13,30 +13,12 @@ import Then
 class TemplateInfoHeaderView: UIView {
     
     // MARK: - Properties
-    private let bgImage = UIImageView().then {
-        $0.image = UIImage(named: "frame_header")
-        $0.backgroundColor = .clear
-    }
-    
-    private let jewelImage = UIImageView().then {
-        $0.image = UIImage(named: "gems")
-        $0.backgroundColor = .clear
-    }
-    
-    private let instaLabel = UILabel().then {
-        $0.text = "캐라 인스타그램에서 다양한 고민 원석을 살펴보세요!"
-        $0.font = .kB3B14
-        $0.textColor = .white
-    }
-    
     private let instaBtn = UIButton().then {
-        $0.backgroundColor = .kYellow1
-        $0.titleLabel?.font = .kB2R16
-        $0.setTitle("공유하기", for: .normal)
-        $0.setTitleColor(.black, for: .normal)
-        $0.layer.cornerRadius = 8
+        $0.setBackgroundImage(UIImage(named: "insta_back"), for: .normal)
+        $0.layer.cornerRadius = 16
+        $0.backgroundColor = .clear
     }
-    
+
     // MARK: - View Life Cycle
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -65,31 +47,12 @@ class TemplateInfoHeaderView: UIView {
 // MARK: - Layout
 extension TemplateInfoHeaderView {
     private func setLayout() {
-        self.addSubviews([bgImage, jewelImage, instaLabel, instaBtn])
-        
-        bgImage.snp.makeConstraints {
-            $0.width.equalTo(342.adjustedW)
-            $0.height.equalTo(168.adjustedW)
-            $0.edges.equalToSuperview()
-        }
-        
-        jewelImage.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(146.adjustedW)
-            $0.height.equalTo(94.adjustedW)
-        }
-        
-        instaLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(90.adjustedW)
-            $0.centerX.equalToSuperview()
-        }
-        
+        self.addSubviews([instaBtn])
+
         instaBtn.snp.makeConstraints {
-            $0.top.equalTo(instaLabel.snp.bottom).offset(13.adjustedW)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(78.adjustedW)
-            $0.height.equalTo(30.adjustedW)
+            $0.width.equalTo(342.adjustedW)
+            $0.height.equalTo(113.adjustedW)
+            $0.edges.equalToSuperview()
         }
     }
 }
