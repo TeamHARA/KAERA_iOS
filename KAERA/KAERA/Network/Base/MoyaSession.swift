e62e3b9a35b71dc6a2d967e35614f6178840c25b
daminoworld <ekadls757@gmail.com>
2024-01-21 23:10:21 +0900

    Add: #195-Create MoyaSession with configuration, interceptor
    - URlsessionConfiguration타입 프로퍼티 생성 후
    - configuratioin의 timeoutIntervalForRequest를 5로 설정하여 요청 대기 시간을 5초로 설정
    - 그리고 AuthInterceptor로 Session의 interceptor로 설정

---
+++ KAERA/KAERA/Network/Base/MoyaSession.swift
@@ -0,0 +1,23 @@
+//
+//  MoyaSession.swift
+//  KAERA
+//
+//  Created by 김담인 on 2024/01/21.
+//
+
+import Foundation
+import Moya
+
+final class MoyaSession {
+    
+    let session: Session = {
+        let configuration = URLSessionConfiguration.af.default
+        configuration.timeoutIntervalForRequest = 5
+        let interceptor = AuthInterceptor.shared
+        return Session(configuration: configuration, interceptor: interceptor)
+    }()
+    
+    static let shared = MoyaSession()
+    
+    private init() { }
+}
