41eb721961f46268fd8f52ee1acb415f025bba68
oy6uns <bacho4235@gmail.com>
2024-01-25 01:28:41 +0900

    Feat: #198-asynchronous processing with future, promise

--- KAERA/KAERA/Scenes/Archive/ViewModel/WorryListViewModel.swift
+++ KAERA/KAERA/Scenes/Archive/ViewModel/WorryListViewModel.swift
@@ -9,46 +9,50 @@ import UIKit
 import Combine
 
 // 뷰 모델로써 데이터의 상태를 가지고 있음
-class WorryListViewModel: ViewModelType {
+final class WorryListViewModel: ViewModelType {
     
     // MARK: - Properties
     private var IdtoImgDict: [Int: String] = [1: "gem_blue_m", 2: "gem_red_m", 3: "gem_orange_m", 4: "gem_green_m", 5: "gem_pink_m", 6: "gem_yellow_m"]
     
-    private var worryUpdateList: [WorryListPublisherModel] = []
-    
-    private var cancellables = Set<AnyCancellable>()
-    
     typealias Input = AnyPublisher<Int, Never>
     typealias Output = AnyPublisher<[WorryListPublisherModel], Error>
     
-    private let output = PassthroughSubject<[WorryListPublisherModel], Error> ()
+    private let output: PassthroughSubject<[WorryListPublisherModel], Error> = .init()
     
-    func transform(input: Input) -> AnyPublisher<[WorryListPublisherModel], Error> {
-        input.sink{[weak self] templateId in
-            self?.getNetworkResponse(templateId)
-        }
-        .store(in: &cancellables)
-        
-        return output.eraseToAnyPublisher()
+    func transform(input: AnyPublisher<Int, Never>) -> AnyPublisher<[WorryListPublisherModel], Error> {
+        input
+            .flatMap { templateId in
+                self.getNetworkResponse(templateId: templateId)
+            }
+            .eraseToAnyPublisher()
     }
     
     // MARK: - Functions
-    private func convertIdtoWorryList(_ data: [Worry]) {
-        worryUpdateList = []
+    private func convertIdtoWorryList(_ data: [Worry]) -> [WorryListPublisherModel] {
+        var worryList: [WorryListPublisherModel] = []
         data.forEach {
             guard let imgName = IdtoImgDict[$0.templateId] else { return }
-            worryUpdateList.append(WorryListPublisherModel(worryId: $0.worryId, templateId: $0.templateId, title: $0.title, period: $0.period, image: UIImage(named: imgName) ?? UIImage() ))
+            worryList.append(WorryListPublisherModel(worryId: $0.worryId, templateId: $0.templateId, title: $0.title, period: $0.period, image: UIImage(named: imgName) ?? UIImage() ))
         }
-        self.output.send(worryUpdateList)
+        return worryList
     }
     
-    private func getNetworkResponse(_ templateId: Int) {
-        ArchiveAPI.shared.getArchiveWorryList(param: templateId) { result in
-            guard let result = result, let data = result.data else {
-                self.output.send(completion: .failure(NSError()))
-                return
+    private func getNetworkResponse(templateId: Int) -> AnyPublisher<[WorryListPublisherModel], Error> {
+        Future<[WorryListPublisherModel], Error> { promise in
+            ArchiveAPI.shared.getArchiveWorryList(param: templateId) { result in
+                switch result {
+                case .success(let response):
+                    if let data = response.data {
+                        let worryList = self.convertIdtoWorryList(data.worry)
+                        promise(.success(worryList))
+                    } else {
+                        promise(.failure(ErrorCase.appError))
+                    }
+                case .failure(let errorCase):
+                    promise(.failure(errorCase))
+                }
             }
-            self.convertIdtoWorryList(data.worry)
         }
+        .eraseToAnyPublisher()
     }
 }
