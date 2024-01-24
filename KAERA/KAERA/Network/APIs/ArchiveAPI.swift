2c2a5965252d35ddfa52be5842a0f7717b863689
oy6uns <bacho4235@gmail.com>
2024-01-25 01:28:09 +0900

    Feat: #198-handling network error and in-app error divergence

--- KAERA/KAERA/Network/APIs/ArchiveAPI.swift
+++ KAERA/KAERA/Network/APIs/ArchiveAPI.swift
@@ -17,9 +17,8 @@ final class ArchiveAPI {
     
     public private(set) var archiveWorryListResponse: GeneralResponse<WorryListModel>?
     
-    
     // MARK: - HomeGemList
-    func getArchiveWorryList(param: Int, completion: @escaping (GeneralResponse<WorryListModel>?) -> () ) {
+    func getArchiveWorryList(param: Int, completion: @escaping (Result<GeneralResponse<WorryListModel>, ErrorCase>) -> Void ) {
         archiveProvider.request(.archiveWorryList(templateId: param)) { [weak self] response in
             switch response {
             case .success(let result):
@@ -27,14 +26,19 @@ final class ArchiveAPI {
                     self?.archiveWorryListResponse = try
                     result.map(GeneralResponse<WorryListModel>?.self)
                     guard let worryList = self?.archiveWorryListResponse else { return }
-                    completion(worryList)
+                    completion(.success(worryList))
                 } catch(let err) {
                     print(err.localizedDescription)
-                    completion(nil)
+                    completion(.failure(.appError))
                 }
             case .failure(let err):
-                print(err.localizedDescription)
-                completion(nil)
+                if let response = err.response {
+                    /// HTTP 상태 코드가 400-500 사이인 경우 appError로 처리
+                    completion(.failure((400...500).contains(response.statusCode) ? .appError : .internetError))
+                } else {
+                    /// 응답이 없는 경우 internetError로 처리
+                    completion(.failure(.internetError))
+                }
             }
         }
     }
